82cf9fc34277a31b14ecbd7608ac4c73d94ade74
olfersl1 <laurens.olfers@hva.nl>
2021-09-24 10:22:10 +0200

    first commit

---
+++ Assignment-1-Trains-project-starter-v21.2/src/main/java/models/FreightWagon.java
@@ -0,0 +1,16 @@
+package models;
+
+public class FreightWagon extends Wagon {
+
+    public int maxWeight;
+
+    public FreightWagon(int wagonId, int maxWeight) {
+        super(wagonId);
+        this.maxWeight = maxWeight;
+
+    }
+
+    public int getMaxWeight() {
+        return this.maxWeight;
+    }
+}
---
+++ Assignment-1-Trains-project-starter-v21.2/src/main/java/models/PassengerWagon.java
@@ -0,0 +1,16 @@
+package models;
+
+
+public class PassengerWagon extends Wagon {
+    public int numberOfSeats;
+
+    public PassengerWagon(int wagonId, int numberOfSeats) {
+        super(wagonId);
+        this.numberOfSeats = numberOfSeats;
+
+    }
+
+    public int getNumberOfSeats() {
+        return this.numberOfSeats;
+    }
+}
